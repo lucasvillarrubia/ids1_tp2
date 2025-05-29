6e76ef5bd75fe4636f427f48ec48d7676ae8117f
viczubieta <mzubieta@fiuba.ar>
2025-05-29 08:42:14 -0300

    agrego archivo corregido

--- backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/user/EmailRepository.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/user/EmailRepository.java
@@ -1,12 +1,14 @@
 package ar.uba.fi.ingsoft1.todo_template.user;
 
 import ar.uba.fi.ingsoft1.todo_template.user.EmailService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "3 - Mail Validation")
 public class EmailRepository {
     @Autowired
     private EmailService emailService;
