231ddae176e44c62935e2ad841788b1d5c354045
AlejoSendra1 <alejo.sendra12@gmail.com>
2025-06-20 20:20:34 -0300

    ParticipationTypeService modulation
    creation of the functions buildCloseFromDTO and buildOpenFromDTO to modulate the building function 'buildFromDTO'

--- backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/match/participationType/ParticipationTypeService.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/match/participationType/ParticipationTypeService.java
@@ -23,28 +23,37 @@ public class ParticipationTypeService {
 
     public ParticipationType buildFromDTO(ParticipationTypeDTO dto) {
         if (dto instanceof CloseDTO closeDTO) {
-            Optional<TeamDTO> teamADTO = Optional.ofNullable(teamService.getTeam(closeDTO.getTeama()));
-            Optional<TeamDTO> teamBDTO = Optional.ofNullable(teamService.getTeam(closeDTO.getTeamb()));
-            if (teamADTO.isEmpty() || teamBDTO.isEmpty()){
-                throw new IllegalArgumentException("Both teams must exist");
-            }
-            Team teama = teamADTO.get().asTeam();
-            Team teamb = teamBDTO.get().asTeam();
-            return new Close(teama, teamb);
+            return buildCloseFromDTO(closeDTO);
         } else if (dto instanceof OpenDTO openDTO) {
-            if (openDTO.getMinPlayersCount() > openDTO.getMaxPlayersCount()){
-                throw new IllegalArgumentException("Minimum player count is bigger than the maximum player count");
-            }
-            HashSet<User> players = new HashSet<>();
-            for(String email: openDTO.getPlayers()){
-                players.add(userService.getUserByEmail(email));
-            }
-
-            return new Open(openDTO.getMinPlayersCount(), openDTO.getMaxPlayersCount(), players); // TODO deberia devolver algun error en casos de invalid players
+            return buildOpenFromDTO(openDTO);
         } else {
             throw new IllegalArgumentException("Unknown participation type");
         }
     }
+
+    private ParticipationType buildCloseFromDTO(CloseDTO dto) {
+        Optional<TeamDTO> teamADTO = Optional.ofNullable(teamService.getTeam(dto.getTeama()));
+        Optional<TeamDTO> teamBDTO = Optional.ofNullable(teamService.getTeam(dto.getTeamb()));
+        if (teamADTO.isEmpty() || teamBDTO.isEmpty()){
+            throw new IllegalArgumentException("Both teams must exist");
+        }
+        Team teama = teamADTO.get().asTeam();
+        Team teamb = teamBDTO.get().asTeam();
+        return new Close(teama, teamb);
+    }
+
+    private ParticipationType buildOpenFromDTO(OpenDTO dto) {
+        HashSet<User> players = new HashSet<>();
+        for(String email: dto.getPlayers()){
+            try{
+                players.add(userService.getUserByEmail(email));
+            } catch (Exception e) {
+                continue;
+            }
+        }
+        return new Open(dto.getMinPlayersCount(), dto.getMaxPlayersCount(), players);
+    }
+
 
     public void updateParticipationType(ParticipationType oldPartType,ParticipationType newPartType){
         newPartType.setId(oldPartType.getId());
