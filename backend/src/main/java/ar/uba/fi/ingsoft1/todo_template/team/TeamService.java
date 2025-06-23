c013ed0b90e72f578e884cbad466594500c4c9b9
Nicoezg <ngruner@fi.uba.ar>
2025-06-23 17:50:34 -0300

    refactor: now checks if user is captain when managing team function checkIfUserIsCaptain does so

--- backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/team/TeamService.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/team/TeamService.java
@@ -3,6 +3,7 @@ package ar.uba.fi.ingsoft1.todo_template.team;
 import java.util.List;
 
 import ar.uba.fi.ingsoft1.todo_template.common.exception.DuplicateEntityException;
+import ar.uba.fi.ingsoft1.todo_template.common.exception.InvalidActionException;
 import ar.uba.fi.ingsoft1.todo_template.user.UserService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,6 +51,8 @@ public class TeamService {
     public TeamDTO updateTeams(String teamName, TeamCreateDTO teamCreateDTO) {
         Team team = teamRepository.findById(teamName).orElseThrow(() -> new EntityNotFoundException("Team not found"));
 
+        checkIfUserIsCaptain(teamName);
+
         Team newTeam = teamCreateDTO.asTeam(userService.getCurrentUserEmail());
 
         if (teamRepository.existsById(newTeam.getName()) && !team.getName().equals(newTeam.getName())) {
@@ -76,6 +79,7 @@ public class TeamService {
         if (!playerName.isEmpty()) {
             throw new IllegalArgumentException("Player name can not be empty");
         }
+        checkIfUserIsCaptain(teamName);
 
         if (team.addPlayer(playerName)) {
             teamRepository.save(team);
@@ -93,6 +97,8 @@ public class TeamService {
             throw new IllegalArgumentException("Can not remove the captain player from the team");
         }
 
+        checkIfUserIsCaptain(teamName);
+
         if (team.removePlayer(playerName)) {
             teamRepository.save(team);
         }
@@ -105,7 +111,16 @@ public class TeamService {
         if (!teamRepository.existsById(teamName)) {
             throw new EntityNotFoundException("Team not found");
         }
-
+        checkIfUserIsCaptain(teamName);
         teamRepository.deleteById(teamName);
     }
+
+    private void checkIfUserIsCaptain(String teamName){
+        TeamDTO team = getTeam(teamName);
+        if (!userService.getCurrentUserEmail().equals(team.captain())) {
+            throw new InvalidActionException("Solo el capitán puede editar el equipo");
+        }
+    }
+
+
 }
